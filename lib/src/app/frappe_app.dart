98972591afa6ad93ef6bf4b9661942b4a98e69c3
Talat El Beick <talatelbeick@gmail.com>
2025-02-01 17:58:24 +0300

    :sparkles: Implement FrappeApp with FrappeDBRepository instance
    - Introduces the `FrappeApp` class to encapsulate Frappe application details.
    - Adds `url`, `name`, `headers`, `useToken`, `token`, and `tokenType` properties to `FrappeApp`.
    - Implements a private `_dio` getter to configure a `Dio` client with the provided settings and headers.
    - Adds a `db` getter, returning an instance of `FrappeDBRepositoryImpl` that uses `FrappeDBRemoteDataSourceImpl` and the `_dio` client.
    - Integrates the `dio` package for HTTP communication.
    - Creates FrappeDBRepository to manage interactions.

--- lib/src/app/frappe_app.dart
+++ lib/src/app/frappe_app.dart
@@ -1,6 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:frappe_sdk/src/db/data/data_source/remote/frappe_db_remote_data_source_impl.dart';
+import 'package:frappe_sdk/src/db/data/repository/frappe_db_repository_impl.dart';
+import 'package:frappe_sdk/src/db/domain/repository/frappe_db_repository.dart';
 import 'package:frappe_sdk/src/utils/token_type.dart';
 
+/// The Frappe application.
 class FrappeApp {
+  /// Creates a new Frappe application.
   const FrappeApp({
     required this.url,
     required this.name,
@@ -10,10 +16,38 @@ class FrappeApp {
     this.tokenType = TokenType.token,
   });
 
+  /// The url of the Frappe application.
   final Uri url;
+
+  /// The name of the Frappe application.
   final String name;
+
+  /// The headers of the Frappe application.
   final Map<String, String> headers;
+
+  /// Whether to use the token or not.
   final bool useToken;
+
+  /// The token of the Frappe application.
   final String? token;
+
+  /// The token type of the Frappe application.
   final TokenType tokenType;
+
+  /// The [FrappeDBRepository] instance.
+  FrappeDBRepository get db => FrappeDBRepositoryImpl(
+        FrappeDBRemoteDataSourceImpl(_dio),
+      );
+
+  Dio get _dio {
+    final Map<String, String> headersCopy = Map<String, String>.from(headers);
+    if (useToken) headersCopy['authorization'] = '${tokenType.value} $token';
+
+    return Dio(
+      BaseOptions(
+        baseUrl: url.toString(),
+        headers: headersCopy,
+      ),
+    );
+  }
 }
