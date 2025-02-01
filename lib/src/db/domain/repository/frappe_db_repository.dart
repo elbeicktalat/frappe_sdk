d4c0dab22c6c4fb5575a74017241541d801b86b6
Talat El Beick <talatelbeick@gmail.com>
2025-02-01 03:38:45 +0300

    :memo: Document FrappeDBRepository methods
    Adds documentation to the methods of the `FrappeDBRepository` interface using Dart's documentation syntax. Each method now has a description and template to explain its purpose, parameters, and behavior.

--- lib/src/db/domain/repository/frappe_db_repository.dart
+++ lib/src/db/domain/repository/frappe_db_repository.dart
@@ -4,17 +4,23 @@ import 'package:frappe_sdk/src/db/domain/utils/typedefs.dart';
 
 /// A repository for interacting with the Frappe database.
 abstract interface class FrappeDBRepository {
+  /// {@template FrappeDBRepository.getDoc}
+  ///
   /// Get a document from the database and return it as a [T] object.
   ///
   /// * [docType] The Frappe `doctype` name.
   /// * [docName] The identifier of the document.
   /// * [fromJson] FromJson function to parse the document.
+  ///
+  /// {@endtemplate}
   Future<T?> getDoc<T extends FrappeDoc>(
     String docType,
     String docName, {
     required T Function(Map<String, dynamic> json) fromJson,
   });
 
+  /// {@template FrappeDBRepository.getDocList}
+  ///
   /// Get a list of documents from the database.
   ///
   /// * [docType] The Frappe `doctype` name.
@@ -26,6 +32,8 @@ abstract interface class FrappeDBRepository {
   /// * [limitStart] The offset to start from.
   /// * [orderBy] Sort results by field and order.
   /// * [groupBy] Group the results by particular field.
+  ///
+  /// {@endtemplate}
   Future<List<T?>?> getDocList<T>(
     String docType, {
     required T Function(Map<String, dynamic> json) fromJson,
@@ -38,44 +46,62 @@ abstract interface class FrappeDBRepository {
     String? groupBy,
   });
 
+  /// {@template FrappeDBRepository.createDoc}
+  ///
   /// Create a new document in the database.
   ///
   /// * [docType] The Frappe `doctype` name.
   /// * [body] The document data.
+  ///
+  /// {@endtemplate}
   Future<T?> createDoc<T extends FrappeDoc>(
     String docType,
     Map<String, dynamic> body,
   );
 
+  /// {@template FrappeDBRepository.updateDoc}
+  ///
   /// Update an existing document in the database.
   ///
   /// * [docType] The Frappe `doctype` name.
   /// * [docName] The identifier of the document.
   /// * [body] The document data.
+  ///
+  /// {@endtemplate}
   Future<T?> updateDoc<T>(
     String docType,
     String docName,
     Map<String, dynamic> body,
   );
 
+  /// {@template FrappeDBRepository.deleteDoc}
+  ///
   /// Delete a document from the database.
   ///
   /// * [docType] The Frappe `doctype` name.
   /// * [docName] The identifier of the document.
+  ///
+  /// {@endtemplate}
   Future<bool> deleteDoc<T>(
     String docType,
     String docName,
   );
 
+  /// {@template FrappeDBRepository.countDoc}
+  ///
   /// Get the count of documents in the database.
   ///
   /// * [docType] The Frappe `doctype` name.
   /// * [filters] The filters to apply.
+  ///
+  /// {@endtemplate}
   Future<int?> countDoc<T>(
     String docType, {
     List<Filter>? filters,
   });
 
+  /// {@template FrappeDBRepository.getLastDoc}
+  ///
   /// Get the last [docType] document from the database.
   ///
   /// * [docType] The Frappe `doctype` name.
@@ -83,6 +109,8 @@ abstract interface class FrappeDBRepository {
   /// * [filters] The filters to apply - SQL AND Operation.
   /// * [orFilters] The filters to apply - SQL OR Operation.
   /// * [orderBy] Sort results by field and order.
+  ///
+  /// {@endtemplate}
   Future<T?> getLastDoc<T extends FrappeDoc>(
     String docType, {
     required T Function(Map<String, dynamic> json) fromJson,
