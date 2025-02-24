3237826ea0fea12903281f443ab3d6b182ce70c9
Talat El Beick <talatelbeick@gmail.com>
2025-02-24 13:08:14 +0300

    :adhesive_bandage: Expose core entities and functionalities through `frappe_sdk` library
    The `frappe_sdk` library now exports key entities and functionalities, making them directly accessible to users of the library. This includes `FrappeApp`, filters, `FrappeDoc`, `FrappeDocStatus`, `FrappeDBRepository`, `typedefs`, `FrappeException`, and `TokenType`. This change facilitates easier integration and usage of the Frappe SDK by consolidating exports.

--- lib/frappe_sdk.dart
+++ lib/frappe_sdk.dart
@@ -1,1 +1,11 @@
 library frappe_sdk;
+
+export 'package:frappe_sdk/src/app/frappe_app.dart';
+export 'package:frappe_sdk/src/db/domain/entity/filter/filter.dart';
+export 'package:frappe_sdk/src/db/domain/entity/filter/filter_operator.dart';
+export 'package:frappe_sdk/src/db/domain/entity/frappe_doc/frappe_doc.dart';
+export 'package:frappe_sdk/src/db/domain/entity/frappe_doc/frappe_doc_status.dart';
+export 'package:frappe_sdk/src/db/domain/repository/frappe_db_repository.dart';
+export 'package:frappe_sdk/src/db/domain/utils/typedefs.dart';
+export 'package:frappe_sdk/src/exception/frappe_exception.dart';
+export 'package:frappe_sdk/src/utils/token_type.dart';
