de7972ea96929666b292515d45f20ea57ca91f1f
Talat El Beick <talatelbeick@gmail.com>
2025-02-24 19:28:06 +0300

    :memo: Add documentation templates to `FrappeCallRepository` methods
    Adds documentation templates to the methods of the `FrappeCallRepository` interface using Dart's documentation syntax. Each method now has a description template to explain its purpose, parameters, and behavior.

--- lib/src/call/domain/repository/frappe_call_repository.dart
+++ lib/src/call/domain/repository/frappe_call_repository.dart
@@ -8,12 +8,16 @@ import 'package:frappe_sdk/src/call/domain/utils/http_methods.dart';
 
 /// The repository interface for the Frappe call.
 abstract interface class FrappeCallRepository {
+  /// {@template FrappeCallRepository.call}
+  ///
   /// Makes request to the specified endpoint.
   ///
   /// * [method] The frappe method path to call.
   /// * [fromJson] The function to parse the response.
   /// * [params] The query parameters to send with the request.
   /// * [httpMethod] The HTTP method to use.
+  ///
+  /// {@endtemplate}
   Future<T?> call<T>(
     String method, {
     required T Function(JSON json) fromJson,
@@ -21,6 +25,8 @@ abstract interface class FrappeCallRepository {
     HttpMethods httpMethod = HttpMethods.get,
   });
 
+  /// {@template FrappeCallRepository.getBalance}
+  ///
   /// Get balance on a specific account.
   ///
   /// * [account] The account to get the balance for.
@@ -33,6 +39,8 @@ abstract interface class FrappeCallRepository {
   /// * [costCenter] The cost center to get the balance for.
   /// * [ignoreAccountPermission] Whether to ignore the account permission.
   /// * [accountType] The account type to get the balance for.
+  ///
+  /// {@endtemplate}
   Future<double?> getBalance({
     String? account,
     DateTime? date,
@@ -46,6 +54,8 @@ abstract interface class FrappeCallRepository {
     String? accountType,
   });
 
+  /// {@template FrappeCallRepository.getFiscalYear}
+  ///
   /// Get current fiscal year.
   ///
   /// * [date] The date to get the fiscal year for.
@@ -55,6 +65,8 @@ abstract interface class FrappeCallRepository {
   /// * [company] The company to get.
   /// * [asMap] Whether to return the fiscal year as a map.
   /// * [fromJson] The function to parse the response.
+  ///
+  /// {@endtemplate}
   Future<T?> getFiscalYear<T>({
     DateTime? date,
     String? fiscalYear,
@@ -65,24 +77,36 @@ abstract interface class FrappeCallRepository {
     T Function(JSON json) fromJson,
   });
 
+  /// {@template FrappeCallRepository.getCompanyDefault}
+  ///
   /// Get company default.
   ///
   /// * [company] The company to get values from.
   /// * [fieldName] The field to get.
+  ///
+  /// {@endtemplate}
   Future<T?> getCompanyDefault<T>({
     String? company,
     String? fieldName,
   });
 
+  /// {@template FrappeCallRepository.getCompanies}
+  ///
   /// Get all companies.
+  ///
+  /// {@endtemplate}
   Future<List<String>> getCompanies();
 
+  /// {@template FrappeCallRepository.getChildren}
+  ///
   /// Get child table values of a document.
   ///
   /// * [docType] The document type.
   /// * [parent] The parent document.
   /// * [fields] The fields to get.
   /// * [filters] The filters to apply.
+  ///
+  /// {@endtemplate}
   Future<List<T?>> getChildren<T>(
     String docType,
     String parent, {
@@ -90,6 +114,8 @@ abstract interface class FrappeCallRepository {
     List<Filter>? filters,
   });
 
+  /// {@template FrappeCallRepository.getStockBalance}
+  ///
   /// Get stock balance.
   ///
   /// * [itemCode] The item code.
@@ -97,6 +123,8 @@ abstract interface class FrappeCallRepository {
   /// * [postingDate] The posting date.
   /// * [withValuationRate] Whether to include valuation rate.
   /// * [withSerialNo] Whether to include serial number.
+  ///
+  /// {@endtemplate}
   Future<T?> getStockBalance<T>({
     required String itemCode,
     required String warehouse,
@@ -105,16 +133,24 @@ abstract interface class FrappeCallRepository {
     bool withSerialNo = false,
   });
 
+  /// {@template FrappeCallRepository.scanBarcode}
+  ///
   /// Search for item by barcode.
   ///
   /// * [barcode] The barcode to search for.
+  ///
+  /// {@endtemplate}
   Future<BarcodeScanResult?> scanBarcode(String barcode);
 
+  /// {@template FrappeCallRepository.getExchangeRate}
+  ///
   /// Get exchange rate.
   ///
   /// * [fromCurrency] The currency to convert from.
   /// * [toCurrency] The currency to convert to.
   /// * [date] The date to get exchange price on.
+  ///
+  /// {@endtemplate}
   Future<double?> getExchangeRate({
     required String fromCurrency,
     required String toCurrency,
