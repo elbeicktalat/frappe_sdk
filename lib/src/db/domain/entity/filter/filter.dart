cdf7b889de5a4eeebd270aad20122f13ee503dd0
Talat El Beick <talatelbeick@gmail.com>
2025-11-11 04:07:12 +0300

    :recycle: Add `Filter` convenience constructors and `toJson` method
    Adds named constructors to the `Filter` class for each `FilterOperator`, simplifying filter creation.
    
    Also, introduces a `toJson` method to convert a `Filter` instance into a JSON-encodable map, which uses the operator's symbol.

--- lib/src/db/domain/entity/filter/filter.dart
+++ lib/src/db/domain/entity/filter/filter.dart
@@ -13,6 +13,61 @@ final class Filter {
     required this.value,
   });
 
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.equal].
+  Filter.equal(String field, Object value)
+      : this(field: field, operator: FilterOperator.equal, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.notEqual].
+  Filter.notEqual(String field, Object value)
+      : this(field: field, operator: FilterOperator.notEqual, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.lessThan].
+  Filter.lessThan(String field, Object value)
+      : this(field: field, operator: FilterOperator.lessThan, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.lessThanOrEqual].
+  Filter.lessThanOrEqual(String field, Object value)
+      : this(field: field, operator: FilterOperator.lessThanOrEqual, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.greaterThan].
+  Filter.greaterThan(String field, Object value)
+      : this(field: field, operator: FilterOperator.greaterThan, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.greaterThanOrEqual].
+  Filter.greaterThanOrEqual(String field, Object value)
+      : this(field: field, operator: FilterOperator.greaterThanOrEqual, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.like].
+  Filter.like(String field, Object value)
+      : this(field: field, operator: FilterOperator.like, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.notLike].
+  Filter.notLike(String field, Object value)
+      : this(field: field, operator: FilterOperator.notLike, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.$in].
+  Filter.in_(String field, Object value)
+      : this(field: field, operator: FilterOperator.$in, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.notIn].
+  Filter.notIn(String field, Object value)
+      : this(field: field, operator: FilterOperator.notIn, value: value);
+
+  /// Creates a new filter with the given field and value.
+  /// The operator will be [FilterOperator.between].
+  Filter.between(String field, Object value)
+      : this(field: field, operator: FilterOperator.between, value: value);
+
   /// The field to filter by.
   final String field;
 
@@ -21,4 +76,13 @@ final class Filter {
 
   /// The value to filter by.
   final Object value;
+
+  /// Converts the filter instance to a JSON encodable map.
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'field': field,
+      'operator': operator.symbol,
+      'value': value,
+    };
+  }
 }
